01158d4650b7f6fdbb10534c1d6fa6014e448d66
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-08-23 02:37:16 +0300

    Header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const menu = document.querySelector('.btn, btn__off');
+const mobileNavigation = document.querySelector('.navigation');
+const main = document.querySelector('.main');
+const checkbox = document.querySelector('.toggle--checkbox');
+
+menu.addEventListener('click', () => {
+  mobileNavigation.classList.toggle('navigation--active');
+});
+
+main.addEventListener('click', () => {
+  if (mobileNavigation.classList.contains('navigation--active')) {
+    mobileNavigation.classList.toggle('navigation--active');
+    checkbox.checked = false;
+  }
+});
+
+const links = document.querySelectorAll('.navigation__link');
+
+links.forEach((item) =>
+  item.addEventListener('click', () => {
+    if (mobileNavigation.classList.contains('navigation--active')) {
+      mobileNavigation.classList.toggle('navigation--active');
+      checkbox.checked = false;
+    }
+  })
+);
